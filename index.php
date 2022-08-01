6f43be94d31c5b89429e9465dac96adea6f9cf73
Alan Muniz <jorgeamr2772@gmail.com>
2022-07-31 22:22:15 -0500

    Rediseño del login y registro de usuarios

--- index.php
+++ index.php
@@ -1,39 +1,51 @@
 <!--1/julio/2022-->
 <html>
     <head>
-        <!--Titulo de la pestaña--> 
-        <title>Login</title>
-        <link rel="stylesheet" href="css/index.css"><!--Conexion con archivo css para dar diseño al html-->
+        <title>Inicio Sesion</title>
+        <!--Conexion con archivo css para dar diseño al html-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/index.css">
         <link rel="icon" href="css/brazil-flag.ico">
     </head>
     <!--Cuerpo de la pagina-->
-    <body>
-     <!--Ingresamos el menu en la parte superior de la pagina-->   
-        <a href="registro.php">
-            <p>No tienes cuenta? Registrate aqui¡</p>
-        </a>
-        <!--Division que contendra los elementos(Titulo y cuerpo) del formulario-->
-        <!--Asiganar class a los elementos sirve para identificarlos en el css y poder dar un diseño especifico-->
-        <div class="center">
-            <!--Encabezado-->
-            <h1>Login</h1>
-            <!--el tag "Form" es para indicar al html que dentro del tag hay un formulario-->
-            <form  method = "post" action="LecturaSolicitud/inicioSesion.php"><!--Action es la accion que realizara el boton de "ingresar" una vez llenado los datos solicitados, en este caso seria redirigir a la pagina principal-->
-                <!--Input de tipo "text" es el recuadro donde el usuario ingresara la informacion de usuario y contraseña-->
-                <div class="txt_field">
-                    <input type="text" placeholder="Usuario" name="form_usuario" required>
-                </div>
-                <div class="txt_field">
-                    <input type="password" placeholder="Contraseña" name="form_contra" required>
-                </div>
-                <!--Input tipo submit es el boton el cual seleccionara el usuario para enviar la informacion previamente ingresada al servidor-->
-                <input type="submit" value="Login" name="form_submit">
+<body class="vh-100 gradient-custom">
+  <div class="container py-3 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card bg-dark text-white" style="border-radius: 1rem;">
+          <div class="card-body p-4 text-center">
+
+            <form class="mb-md-5 mt-md-4 pb-5" method = "post" action="LecturaSolicitud/inicioSesion.php">
+
+              <h2 class="fw-bold mb-2 text-uppercase">Iniciar Sesion</h2>
+              <p class="text-white-50 mb-5">Porfavor ingresa tu usuario y contraseña!</p>
+
+              <div class="form-outline form-white mb-4">
+                <input type="text" name="form_usuario" class="form-control form-control-lg" required>
+                <label class="form-label" for="typeEmailX">Usuario</label>
+              </div>
+
+              <div class="form-outline form-white mb-4">
+                <input type="password" name="form_contra" class="form-control form-control-lg" required>
+                <label class="form-label" for="typePasswordX">Contraseña</label>
+              </div>
+
+              <input class="btn btn-outline-light btn-lg px-5" value="Inicio Sesion" name="form_submit" type="submit"/>
+
             </form>
-            <?php
-            //Agregar aviso cuando el usuario y contraseña sean incorrectos
-            ?>
+
+            <div>
+              <p class="mb-0">No tienes cuenta?
+                <a href="registro.php" class="text-white-50 fw-bold">Registrate</a>
+              </p>
+            </div>
+
+          </div>
         </div>
-    </body>
+      </div>
+    </div>
+  </div>
+</body>
 </html>
 
 <!----> 
--- registro.php
+++ registro.php
@@ -1,33 +1,58 @@
-<!--15/julio/2022-->
+<!--1/julio/2022-->
 <html>
     <head>
         <!--Titulo de la pestaña--> 
         <title>Registro</title>
-        <link rel="stylesheet" href="css/registro.css"><!--Conexion con archivo css para dar diseño al html-->
+        <!--Conexion con archivo css para dar diseño al html-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/registro.css">
         <link rel="icon" href="css/brazil-flag.ico">
     </head>
     <!--Cuerpo de la pagina-->
-    <body>
-        <a href="index.php">
-            <p>Ya tienes cuenta? Inicia sesion AQUI¡</p>
-        </a>
-        <!--Division que contendra los elementos(Titulo y cuerpo) del formulario-->
-        <!--Asignar class a los elementos sirve para identificarlos en el css y poder dar un diseño especifico-->
-        <div class="center">
-            <!--Encabezado-->
-            <h1>Register Now</h1>
-            <!--el tag "Form" es para indicar al html que dentro del tag hay un formulario-->
-            <form  action="empresa.php"><!--Action es la accion que realizara el boton de "ingresar" una vez llenado los datos solicitados, en este caso seria redirigir a la pagina principal-->
-                <!--Input de tipo "text" es el recuadro donde el usuario ingresara la informacion de usuario y contraseña-->
-                <div class="txt_field">
-                    <input type="text" placeholder="Usuario" required>
-                </div>
-                <div class="txt_field">
-                    <input type="text" placeholder="Contraseña" required>
-                </div>
-                <!--Input tipo submit es el boton el cual seleccionara el usuario para enviar la informacion previamente ingresada al servidor-->
-                <input type="submit" value="Ingresar">
+<body class="vh-100 gradient-custom">
+  <div class="container py-3 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card bg-dark text-white" style="border-radius: 1rem;">
+          <div class="card-body p-4 text-center">
+
+            <form class="mb-md-3 mt-md-4 pb-3" method = "post" action="LecturaSolicitud/inicioSesion.php">
+
+              <h2 class="fw-bold mb-2 text-uppercase">Registro</h2>
+              <p class="text-white-50 mb-5">Porfavor ingresa los datos solicitados</p>
+            
+              <div class="form-outline form-white mb-4">
+                <input type="text" name="form_usuario" class="form-control form-control-lg" placeholder="Nombre" required>
+              </div>
+
+              <div class="form-outline form-white mb-4">
+                <input type="text" name="form_usuario" class="form-control form-control-lg" placeholder="Apellido" required>
+              </div>
+
+              <div class="form-outline form-white mb-4">
+                <input type="text" name="form_usuario" class="form-control form-control-lg" placeholder="Nombre Usuario" required>
+              </div>
+
+              <div class="form-outline form-white mb-4">
+                <input type="password" name="form_contra" class="form-control form-control-lg" placeholder="Contraseña" required>
+              </div>
+
+              <input class="btn btn-outline-light btn-lg px-5" value="Enviar" name="form_submit" type="submit"/>
+
             </form>
+
+            <div>
+              <p class="mb-0">Ya tienes cuenta?
+                <a href="index.php" class="text-white-50 fw-bold">Inicia Sesion</a>
+              </p>
+            </div>
+
+          </div>
         </div>
-    </body>
+      </div>
+    </div>
+  </div>
+</body>
 </html>
+
+<!----> 
